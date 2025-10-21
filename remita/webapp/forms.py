b0d10ebae9a1924837460563c2767f31a8f1fb03
Chifula <chichinambela2@gmail.com>
2025-10-21 14:24:34 +0200

    removed vendor_id

--- remita/webapp/forms.py
+++ remita/webapp/forms.py
@@ -9,26 +9,26 @@ class BankDetailsForm(ModelForm):
     def __int__(self, *args, **kwargs):
         super(BankDetailsForm, *args, **kwargs)
         self.fields['account_no'].label = "Account Number"
-        self.fields['vendor_id'].label = "Vendor ID"
+        #self.fields['vendor_id'].label = "Vendor ID"
         self.fields['account_name'].label = "Account Name"
-        self.fields['vendor_mobile_number'].label = "Vendor Mobile Number"
-        self.fields['vendor_email'].label = "Vendor Email"
+        self.fields['vendor_mobile_number'].label = "Beneficiary Mobile Number"
+        self.fields['vendor_email'].label = "Beneficiary Email"
         self.fields['bank_code'].label = "Bank Code"
 
 
     class Meta:
         model = BankDetails
-        fields = ['account_no', 'account_name', 'vendor_id', 'vendor_email', 'vendor_mobile_number', 'bank_name',
+        fields = ['account_no', 'account_name', 'vendor_email', 'vendor_mobile_number', 'bank_name',
                    'bank_code']
         error_messages = {
             'account_no': {
                 'required': 'Please enter the Account Number.',
                 'null': 'The Account Number entered is invalid.',
             },
-            'vendor_id': {
-                'required': 'Please enter the Vendor ID.',
-                'null': 'No Vendor available with Vendor ID entered.',
-            },
+            # 'vendor_id': {
+            #     'required': 'Please enter the Vendor ID.',
+            #     'null': 'No Vendor available with Vendor ID entered.',
+            # },
             'account_name': {
                 'required': 'Please enter the account name.',
                 'null': 'The Account Name cannot be null.',
